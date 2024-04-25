2b9cead1485d36b34d14954b98bb6620fc41bd66
JP <juanpabloduette@gmail.com>
2024-04-24 22:35:16 -0300

    modified showcart

--- js/js.js
+++ js/js.js
@@ -26,21 +26,22 @@ const showProducts = async () => {
 		<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
 		<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
 		<g id="SVGRepo_iconCarrier">
-		  <style type="text/css">
-			.st0{fill:none;stroke:#c0c0c0;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
-			.st1{fill:none;stroke:#c0c0c0;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;stroke-dasharray:3;}
-			.st2{fill:none;stroke:#c0c0c0;stroke-width:2;stroke-linejoin:round;stroke-miterlimit:10;}
-			.st3{fill:none;stroke:transparent;} /* Cambiamos el color de la línea a transparente */
-		  </style>
-		  <path class="st0" d="M9,13V7c0-2.2,1.8-4,4-4h0c2.2,0,4,1.8,4,4v6"></path>
-		  <circle class="st0" cx="23" cy="22" r="7"></circle>
-		  <!-- Modificamos el signo dentro del círculo para que sea un signo más -->
-		  <line class="st0" x1="20" y1="22" x2="26" y2="22"></line>
-		  <line class="st0" x1="23" y1="19" x2="23" y2="25"></line>
-		  <polyline class="st0" points="23,29 3,29 5,9 21,9 21.6,15.1 "></polyline>
-		  <rect y="-144" class="st3" width="536" height="680"></rect>
+			<style type="text/css">
+				.st0{fill:none;stroke:#c0c0c0;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
+				.st1{fill:none;stroke:#c0c0c0;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;stroke-dasharray:3;}
+				.st2{fill:none;stroke:#c0c0c0;stroke-width:2;stroke-linejoin:round;stroke-miterlimit:10;}
+				.st3{fill:none;stroke:transparent;} /* Cambiamos el color de la línea a transparente */
+			</style>
+			<path class="st0" d="M9,13V7c0-2.2,1.8-4,4-4h0c2.2,0,4,1.8,4,4v6"></path>
+			<circle class="st0" cx="23" cy="22" r="7"></circle>
+			<!-- Modificamos el signo dentro del círculo para que sea un signo más -->
+			<line class="st0" x1="20" y1="22" x2="26" y2="22"></line>
+			<line class="st0" x1="23" y1="19" x2="23" y2="25"></line>
+			<!-- Cambiamos las puntas del bolso para que estén redondeadas -->
+			<polyline class="st0" points="23,29 3,29 5,9 21,9 21.6,15.1 " stroke-linecap="round"></polyline>
+			<rect y="-144" class="st3" width="536" height="680"></rect>
 		</g>
-	  </svg>`;
+	</svg>`;
 		addProduct.className = "btn-add";
 		content.append(addProduct);
 
