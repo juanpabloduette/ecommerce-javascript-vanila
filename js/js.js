b5f250291b194472ab149f650b3d60e87af458ec
JP <juanpabloduette@gmail.com>
2024-05-19 21:36:16 -0300

    fixed search bar responsive

--- js/js.js
+++ js/js.js
@@ -8,10 +8,11 @@ const productsCategoriesTitle = document.getElementById(
 );
 const navBar = document.getElementById("navbar");
 const home = document.getElementById("home");
+const search = document.getElementById("search");
+const searchProducts = document.querySelector(".search__products");
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 const showProducts = async () => {
-	
 	const URL_API = "./src/api/api.json";
 	const response = await fetch(URL_API);
 	const data = await response.json();
@@ -29,9 +30,10 @@ const showProducts = async () => {
 		return productsAleatories;
 	}
 	navBar.addEventListener("click", (e) => {
-		e.target.textContent.toUpperCase() !== e.target.textContent &&
+		(e.target.textContent.toUpperCase() !== e.target.textContent)&&(e.target.textContent.length < 200) &&
 			(productsCategoriesTitle.innerText = e.target.textContent);
-
+			searchProducts.innerHTML = "";
+			search.value = "";
 		switch (e.target.textContent) {
 			case "Anime":
 				const productsAnime = data.filter(
@@ -246,8 +248,9 @@ const showProducts = async () => {
 					(product) => product.target === "lionel-messi"
 				);
 				return renderProducts(productsMessi);
-
 			default:
+				// console.log('default')
+				// console.log(e.target.textContent.length)
 				break;
 		}
 	});
@@ -265,27 +268,9 @@ const showProducts = async () => {
 			`;
 			showProductsContent.append(article);
 			const addProduct = document.createElement("button");
-			addProduct.innerHTML = `Agregar al carrito <svg version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve" width="22px" height="22px" fill="#c0c0c0" stroke="#c0c0c0">
-			<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-			<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-			<g id="SVGRepo_iconCarrier">
-				<style type="text/css">
-					.st0{fill:none;stroke:#c0c0c0;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
-					.st1{fill:none;stroke:#c0c0c0;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;stroke-dasharray:3;}
-					.st2{fill:none;stroke:#c0c0c0;stroke-width:2;stroke-linejoin:round;stroke-miterlimit:10;}
-					.st3{fill:none;stroke:transparent;}
-				</style>
-				<path class="st0" d="M9,13V7c0-2.2,1.8-4,4-4h0c2.2,0,4,1.8,4,4v6"></path>
-				<circle class="st0" cx="23" cy="22" r="7"></circle>
-				<line class="st0" x1="20" y1="22" x2="26" y2="22"></line>
-				<line class="st0" x1="23" y1="19" x2="23" y2="25"></line>
-				<polyline class="st0" points="23,29 3,29 5,9 21,9 21.6,15.1 " stroke-linecap="round"></polyline>
-				<rect y="-144" class="st3" width="536" height="680"></rect>
-			</g>
-		</svg>`;
+			addProduct.innerHTML = svgCart;
 			addProduct.className = "btn-add";
 			article.append(addProduct);
-
 			addProduct.addEventListener("click", (e) => {
 				const repeatProduct = cart.some(
 					(repeatProduct) => repeatProduct.id === product.id
@@ -340,15 +325,16 @@ const showProducts = async () => {
 	renderProducts(productsAleatories(data));
 
 	/* BARRA DE BUSQUEDA*/
-	const search = document.getElementById("search");
 	search.addEventListener("input",(e) => {
+		searchProducts.innerHTML = "";
+		// search.addEventListener("blur",()=>{
+		// 	searchProducts.innerHTML = "";
+		// })
 		productsCategoriesTitle.innerText = "Busqueda personalizada"
-			// if(e.target.value !== ""){}
 			if(e.target.value === ""){
 				productsCategoriesTitle.innerText = "Productos destacados";
 				renderProducts(productsAleatories(data));
-			}
-			if(e.target.value === " "){
+			}else if(e.target.value === " "){
 				showProductsContent.innerHTML = "Ingrese datos para la busqueda o seleccione una opción desde CATEGORIAS en el menú";
 			}else {
 				const filteredData = data.filter(item => 
@@ -358,7 +344,12 @@ const showProducts = async () => {
 					||
 					item.target.toLowerCase().includes(e.target.value.toLowerCase())
 				);
-				(filteredData.length == 0) ? showProductsContent.innerHTML = "No se encontraron resultados" : renderProducts(filteredData);
+				(filteredData.length == 0)
+				? 
+				showProductsContent.innerHTML = "No se encontraron resultados relacionados con la búsqueda"
+				:
+				renderProducts(filteredData);
+				searchProducts.innerHTML = `"${filteredData.length}" productos encontrados`
 			}
 	})
 };
---
+++ js/svgcart.js
@@ -0,0 +1,18 @@
+const svgCart = `Agregar al carrito <svg version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve" width="22px" height="22px" fill="#c0c0c0" stroke="#c0c0c0">
+<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+<g id="SVGRepo_iconCarrier">
+    <style type="text/css">
+        .st0{fill:none;stroke:#c0c0c0;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
+        .st1{fill:none;stroke:#c0c0c0;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;stroke-dasharray:3;}
+        .st2{fill:none;stroke:#c0c0c0;stroke-width:2;stroke-linejoin:round;stroke-miterlimit:10;}
+        .st3{fill:none;stroke:transparent;}
+    </style>
+    <path class="st0" d="M9,13V7c0-2.2,1.8-4,4-4h0c2.2,0,4,1.8,4,4v6"></path>
+    <circle class="st0" cx="23" cy="22" r="7"></circle>
+    <line class="st0" x1="20" y1="22" x2="26" y2="22"></line>
+    <line class="st0" x1="23" y1="19" x2="23" y2="25"></line>
+    <polyline class="st0" points="23,29 3,29 5,9 21,9 21.6,15.1 " stroke-linecap="round"></polyline>
+    <rect y="-144" class="st3" width="536" height="680"></rect>
+</g>
+</svg>`
